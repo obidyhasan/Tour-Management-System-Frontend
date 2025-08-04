77de4e13ea471ce4624849b9583965d346239ba8
obidyhasan <obidyhasan@gmail.com>
2025-08-04 10:51:14 +0600

    setup project & folder structure

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="mx-auto container space-y-8 px-4 py-16">
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+        dignissimos fugit pariatur cumque, alias quos sit corporis enim quae
+        neque ratione voluptatibus adipisci ea odit perspiciatis perferendis
+        aperiam excepturi suscipit?
+      </h1>
+    </div>
+  );
+};
+
+export default About;
